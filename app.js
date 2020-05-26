8f65362044d9c75edc3994fe62d5361d0b1c9f3d
GO-INHO <65935021+GO-INHO@users.noreply.github.com>
2020-05-26 22:18:35 +0900

    초기 커밋

---
+++ app.js
@@ -0,0 +1,73 @@
+var albumBucketName = 'si-warrant-ih';
+var bucketRegion = 'ap-northeast-2';
+var IdentityPoolId = 'ap-northeast-2:dd8a17c7-c019-491f-9082-445ea39720b7';
+
+AWS.config.update({
+  region: bucketRegion,
+  credentials: new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: IdentityPoolId
+  })
+});
+
+var s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  params: {Bucket: albumBucketName}
+});
+function createAlbum(albumName) {
+    albumName = albumName.trim();
+    if (!albumName) {
+      return alert('Album names must contain at least one non-space character.');
+    }
+    if (albumName.indexOf('/') !== -1) {
+      return alert('Album names cannot contain slashes.');
+    }
+    var albumKey = encodeURIComponent(albumName) + '/';
+    s3.headObject({Key: albumKey}, function(err, data) {
+      if (!err) {
+        return alert('Album already exists.');
+      }
+      if (err.code !== 'NotFound') {
+        return alert('There was an error creating your album: ' + err.message);
+      }
+      s3.putObject({Key: albumKey}, function(err, data) {
+        if (err) {
+          return alert('There was an error creating your album: ' + err.message);
+        }
+        alert('Successfully created album.');
+        viewAlbum(albumName);
+      });
+    });
+  }
+  function addPhoto(albumName) {
+    var files = document.getElementById("photoupload").files;
+    if (!files.length) {
+      return alert("Please choose a file to upload first.");
+    }
+    var file = files[0];
+    var fileName = file.name;
+    var albumPhotosKey = encodeURIComponent(albumName) + "//";
+  
+    var photoKey = albumPhotosKey + fileName;
+  
+    // Use S3 ManagedUpload class as it supports multipart uploads
+    var upload = new AWS.S3.ManagedUpload({
+      params: {
+        Bucket: albumBucketName,
+        Key: photoKey,
+        Body: file,
+        ACL: "public-read"
+      }
+    });
+  
+    var promise = upload.promise();
+  
+    promise.then(
+      function(data) {
+        alert("Successfully uploaded photo.");
+        viewAlbum(albumName);
+      },
+      function(err) {
+        return alert("There was an error uploading your photo: ", err.message);
+      }
+    );
+  }
